eb55782708939d572883c4b7c56f5209b6a76237
Matty Mason <matty.mason@ipsos.com>
2026-04-17 14:30:40 +0100

    Remove cdm dependency

--- frontend/js/pages/radial.js
+++ frontend/js/pages/radial.js
@@ -1,5 +1,5 @@
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
-
+//import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import * as d3 from "d3";
 export function mountRadial(container, user) {
   renderLayout(container);
   initCardCarousel(container);
