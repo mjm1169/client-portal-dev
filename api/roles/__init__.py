ca65825a32b649bb2aa9dde3162f2f67bac46ef9
Matty Mason <matty.mason@ipsos.com>
2026-04-20 13:38:41 +0100

    change config7

--- api/roles/__init__.py
+++ api/roles/__init__.py
@@ -1,11 +1,27 @@
 import azure.functions as func
 import json
+import base64
+
+ALLOWED_EMAILS = {
+    "matthewmason202@gmail.com",
+    "matty.mason@ipsos.com",
+    "m.mason@karianandbox.com"
+}
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse(
-        json.dumps({
-            "roles": ["authenticated"]
-        }),
-        mimetype="application/json",
-        status_code=200
-    )
+    principal_header = req.headers.get("x-ms-client-principal", "")
+    if not principal_header:
+        return func.HttpResponse(json.dumps({"roles": []}), mimetype="application/json")
+
+    try:
+        principal = json.loads(base64.b64decode(principal_header).decode("utf-8"))
+        email = principal.get("userDetails", "").lower()
+    except Exception:
+        return func.HttpResponse(json.dumps({"roles": []}), mimetype="application/json")
+
+    if email in ALLOWED_EMAILS:
+        roles = ["authenticated"]
+    else:
+        roles = []
+
+    return func.HttpResponse(json.dumps({"roles": roles}), mimetype="application/json")
