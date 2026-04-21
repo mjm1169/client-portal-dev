f1f63558d7fc1e91c9e2479f45b9261cb66db484
Matty Mason <matty.mason@ipsos.com>
2026-04-21 08:44:36 +0100

    2104_3

--- api/roles/__init__.py
+++ api/roles/__init__.py
@@ -9,32 +9,9 @@ ALLOWED_EMAILS = {
 }
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    email = ""
-    principal_header = req.headers.get("x-ms-client-principal", "")
-    if principal_header:
-        try:
-            principal = json.loads(base64.b64decode(principal_header).decode("utf-8"))
-            email = principal.get("userDetails", "").lower()
-        except Exception:
-            pass
-    else:
-        try:
-            body = req.get_json()
-            email = body.get("userDetails", "").lower()
-        except Exception:
-            pass
-
-    if not email:
-        return func.HttpResponse(
-            json.dumps({"roles": []}),
-            mimetype="application/json",
-            status_code=200
-        )
-
-    roles = ["appUser"] if email in ALLOWED_EMAILS else []
-
+    # TEMP DIAGNOSTIC: always return appUser to test if rolesSource mechanism works
     return func.HttpResponse(
-        json.dumps({"roles": roles}),
+        json.dumps({"roles": ["appUser"]}),
         mimetype="application/json",
         status_code=200
     )
